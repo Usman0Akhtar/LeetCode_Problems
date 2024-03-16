786057217409258edaf2177989ef353a786702a2
Usman <146363875+Usman0Akhtar@users.noreply.github.com>
2024-03-16 21:56:59 +0000

    Add files via upload

---
+++ Daily/sixteenth_march_2024.java
@@ -0,0 +1,30 @@
+package Daily;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class sixteenth_march_2024 {
+    public static void main(String[] args) {
+        
+        int [] nums = new ;
+        System.out.println(findMaxLength(nums));
+    }
+    public static int findMaxLength(int[] nums) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int counter = 0;
+        int subArrayLength = 0;
+        for (int i = 0; i < len; i++) {
+            counter += nums[i] == 0 ? -1 : 1;
+            if (counter == 0) {
+                subArrayLength = i + 1;
+            } else if (map.containsKey(counter)) {
+                subArrayLength = Math.max(subArrayLength, i - map.get(counter));
+            } else {
+                map.put(counter, i);
+            }
+        }
+        return subArrayLength;
+    }
+}
