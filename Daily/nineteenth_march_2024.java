8395cabf303b3cc0682c95a5caaf6a2a774cb88c
usman <146363875+Usman0Akhtar@users.noreply.github.com>
2024-03-19 22:10:31 +0000

    Add files via upload

---
+++ Daily/nineteenth_march_2024.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        int max = 0;
+        int maxCount = 0;
+        for(char task : tasks) {
+            freq[task-'A']++;
+            if(max==freq[task-'A']) {
+                maxCount++;
+            }
+            else if(max < freq[task -'A']) {
+                max = freq[task-'A'];
+                maxCount = 1;
+            }
+        }
+        int gapCount = max - 1;
+        int gapLength = n -(maxCount - 1);
+        int empty = gapCount * gapLength;
+
+            
+        int availableTasks = tasks.length - max*maxCount;
+        int result = Math.max(0, empty - availableTasks);
+
+        return tasks.length+result;
+    }
+}
