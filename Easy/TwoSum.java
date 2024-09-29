0897602a82f5b0231bfa342cab5ba6d6079280fd
usman <146363875+Usman0Akhtar@users.noreply.github.com>
2024-09-29 12:57:23 +0100

    Update TwoSum.java

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -1,17 +1,19 @@
 //38ms runtime
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int len = nums.length;
-        for (int i = 0; i < len - 1; i++) 
-        {
-            for (int j = i + 1; j < len; j++) 
-            {
-                if (nums[i] + nums[j] == target) 
-                {
-                    return new int[]{i, j};
-                }
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
             }
+
+            prevMap.put(num, i);
         }
-        return new int[]{};
+
+        return new int[] {};
     }
 }
